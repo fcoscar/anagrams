a2ced2496433418ce78dab289a434aa9ed593a5e
Oscar <oscarjr01.13@gmail.com>
2021-10-01 21:07:40 -0400

    mayusculas

--- main.py
+++ main.py
@@ -1,33 +1,36 @@
 import time
 
+
 def get_words():
     file = open("wordlist.txt", "r")
     words = file.readlines()
     file.close()
     return words
 
+
 def get_anagrams(words):
     dic = {}
     for w in words:
         w = str(w).upper()
-        w = w.replace("\n","") #CAT | ATC
-        word = ''.join(sorted(w)) #ACT | ACT
+        w = w.replace("\n", "")  # CAT | ATC
+        word = ''.join(sorted(w))  # ACT | ACT
         if word not in dic:
-            dic[word] = w #"ACT":"CAT"
+            dic[word] = w  # "ACT":"CAT"
         else:
-            dic[word] += ','+w #ACT": "CAT","ATC"
+            dic[word] += ',' + w  # ACT": "CAT","ATC"
     return dic
+
 
 def delete_when_one(dic):
     og_dic = dict(dic)
-    for k,word in dic.items():
+    for k, word in dic.items():
         if "," not in word:
             og_dic.pop(k)
     return list(og_dic.values())
 
 
 # tic = time.perf_counter()
-start = time.time()
+# start = time.time()
 words = get_words()
 anagrams = get_anagrams(words)
 final_list = delete_when_one(anagrams)
