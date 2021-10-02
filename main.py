ba8970a0f201c3ae49c4941f7f90ba77c3846a48
Oscar <oscarjr01.13@gmail.com>
2021-10-01 21:06:23 -0400

    mayusculas

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import time
+
 def get_words():
     file = open("wordlist.txt", "r")
     words = file.readlines()
@@ -7,6 +9,7 @@ def get_words():
 def get_anagrams(words):
     dic = {}
     for w in words:
+        w = str(w).upper()
         w = w.replace("\n","") #CAT | ATC
         word = ''.join(sorted(w)) #ACT | ACT
         if word not in dic:
@@ -20,9 +23,18 @@ def delete_when_one(dic):
     for k,word in dic.items():
         if "," not in word:
             og_dic.pop(k)
-    return og_dic
+    return list(og_dic.values())
 
+
+# tic = time.perf_counter()
+start = time.time()
 words = get_words()
 anagrams = get_anagrams(words)
 final_list = delete_when_one(anagrams)
+for anagram in final_list:
+    print(anagram)
 print(len(final_list))
+# toc = time.perf_counter()
+# end = time.time()
+# print(toc-tic)
+# print(end-start)
--- tests.py
+++ tests.py
@@ -2,7 +2,6 @@ import unittest
 
 from main import get_words, get_anagrams, delete_when_one
 
-
 class MyTestCase(unittest.TestCase):
     def test_get_words(self):
         self.assertEqual(338882, len(get_words()))  # add assertion here
@@ -15,5 +14,9 @@ class MyTestCase(unittest.TestCase):
     def test_delete_when_one(self):
         result = delete_when_one({'ACT': 'CAT,ATC', 'CPT': 'TCP'})
         self.assertEqual({'ACT': 'CAT,ATC'},result)
+
+    def test_caps(self):
+        result = get_anagrams(["cAt","ATC","TCP"])
+        self.assertEqual({'ACT': 'CAT,ATC', 'CPT': 'TCP'},result)
 if __name__ == '__main__':
     unittest.main()
